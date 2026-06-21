a1df564fdb468db02794dde1f25a0a6a2dc0c555
Bale-e <valentina.rios08@inacapmail.cl>
2026-06-21 17:17:41 -0400

    dscd
    daas

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+from pymongo import MongoClient
+
+import datetime
+import subprocess 
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+client: MongoClient = MongoClient(MONGO_URI)
+
+try:
+    print("Estableciendo conexión...⏳")
+    client.admin.command("ping")
+    print("Conexión establecida 😊")
+except:
+    print("❌ ERROR EN LA CONEXIÓN")
+    exit(code=1)
+
+db = client["ti3032_u3"]
+collection = db["test"]
